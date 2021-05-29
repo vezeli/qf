1523f4af64b66086e4794f5e7a168c5b86eb730a
Velibor Zeli <velibor@mech.kth.se>
2021-05-29 22:38:15 +0200

    Split into private and public/add annotation

--- dynamic_hedge.py
+++ dynamic_hedge.py
@@ -1,81 +1,109 @@
+from typing import Callable, Tuple
+
 import numpy as np
 
 import model
 
-# f :: (a, a, a, a, a, a, a) -> a
-k    = lambda xs: xs[0]
-m    = lambda xs: xs[1]
-sig  = lambda xs: xs[2]
-r    = lambda xs: xs[3]
-t    = lambda xs: xs[4]
-tend = lambda xs: xs[5]
-dt   = lambda xs: xs[6]
-
-# g :: (a, a, a, a) -> a
-s    = lambda xs: xs[0]
-c    = lambda xs: xs[1]
-dcds = lambda xs: xs[2]
-v    = lambda xs: xs[3]
-
-per_year = lambda x: round(x/365, 5)
+# types
+SIM_PARAMETERS = Tuple[float, float, float, float, float, float, float]
+RESULT = Tuple[float, float, float, float]
+HF1 = Callable[[SIM_PARAMETERS], float]
+HF2 = Callable[[RESULT], float]
 
 
-def increment_time(xs):
-    return k(xs), m(xs), sig(xs), r(xs), t(xs)+dt(xs), tend(xs), dt(xs)
+def set_parameters(
+        k: float, m: float, v: float, r: float, t: int, tend: int, dt: int
+        ) -> SIM_PARAMETERS:
+    """Formats parameters.
+
+    Parameters
+    ----------
+    k : strike price [$]
+    m : drift coefficient [%]
+    v : volatility [%]
+    r : risk-free interest rate [%]
+    t : time since establishing contract [days]
+    tend : contract duration [days]
+    dt : simulation time increment [days]
+    """
+    rs = map(_percentage, [m, v, r])
+    ts = map(_per_year, [t, tend, dt])
+    return (k, *rs, *ts)
 
 
-def append(xs, ys):
+def start(initial_price: float, ps: SIM_PARAMETERS) -> np.ndarray:
+    """Starts simulation."""
+    result = _run(_gen_stonk_gbm(initial_price, ps), ps, np.array([]))
+    return np.round(result, 2)
+
+
+def _gen_stonk_gbm(s, ps):
+    args = _m(ps), _sig(ps), _dt(ps)
+    yield s
+    yield from _gen_stonk_gbm(s + model.ds(s, *args), ps)
+
+
+def _run(gs, ps, rs):
+    if _t(ps) > _tend(ps):
+        return rs
+    else:
+        r = _compute(next(gs), ps, rs)
+        rs = _run(gs, _increment_time(ps), _append(rs, r))
+        return rs
+
+
+def _compute(s, ps, rs):
+    v = _cash_balance(_bsm(s, ps), _tail(rs), ps)
+    return v
+
+
+def _cash_balance(rn, rnm, ps):
+    sn, cn, dn = rn
+    _, _, dnm, vnm = rnm
+    vn = (dn - dnm)*sn + vnm*np.e**(_r(ps)*_dt(ps))
+    return sn, cn, dn, vn
+
+
+def _bsm(s, ps):
+    c, d = _evaluate_bsm(s, ps)
+    return s, c, d
+
+
+def _evaluate_bsm(s, ps):
+    args = s, _k(ps), _sig(ps), _r(ps), _t(ps), _tend(ps)
+    return model.c(*args), model.delta(*args)
+
+
+_k:    HF1 = lambda ps: ps[0]
+_m:    HF1 = lambda ps: ps[1]
+_sig:  HF1 = lambda ps: ps[2]
+_r:    HF1 = lambda ps: ps[3]
+_t:    HF1 = lambda ps: ps[4]
+_tend: HF1 = lambda ps: ps[5]
+_dt:   HF1 = lambda ps: ps[6]
+
+_s:    HF2 = lambda ps: ps[0]
+_c:    HF2 = lambda ps: ps[1]
+_dcds: HF2 = lambda ps: ps[2]
+_v:    HF2 = lambda ps: ps[3]
+
+_per_year: Callable[[int], float] = lambda x: x/365
+_percentage: Callable[[float], float] = lambda x: x/100
+
+
+def _increment_time(ps):
+    return _k(ps), _m(ps), _sig(ps), _r(ps), _t(ps)+_dt(ps), _tend(ps), _dt(ps)
+
+
+def _append(xs, ys):
     if np.size(xs) == 0:
         return np.array([ys])
     else:
         return np.vstack([xs, ys])
 
 
-def gen_stonk_gbm(s, xs):
-    args = m(xs), sig(xs), dt(xs)
-    yield s
-    yield from gen_stonk_gbm(s + model.ds(s, *args), xs)
-
-
-def evaluate_bsm(s, xs):
-    args = s, k(xs), sig(xs), r(xs), t(xs), tend(xs)
-    return model.c(*args), model.delta(*args)
-
-
-def bsm(s, xs):
-    c, d = evaluate_bsm(s, xs)
-    return s, c, d
-
-
-def tail(rs):
+def _tail(rs):
     try:
         return rs[-1]
     except IndexError:
         return np.array([0, 0, 0, 0])
-
-
-def cash_balance(rn, rnm, xs):
-    sn, cn, dn = rn
-    _, _, dnm, vnm = rnm
-    vn = (dn - dnm)*sn + vnm*np.e**(r(xs)*dt(xs))
-    return sn, cn, dn, vn
-
-
-def compute(s, xs, rs):
-    v = cash_balance(bsm(s, xs), tail(rs), xs)
-    return v
-
-
-def run(gs, xs, rs):
-    if t(xs) > tend(xs):
-        return rs
-    else:
-        r = compute(next(gs), xs, rs)
-        rs = run(gs, increment_time(xs), append(rs, r))
-        return rs
-
-
-def start(s0, xs):
-    gs = gen_stonk_gbm(s0, xs)
-    result = run(gs, xs, np.array([]))
-    return np.round(result, 2)
